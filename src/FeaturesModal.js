f776732636fc57df470aa1acef0527ae67e92ca0
Roger Schaer <roger.schaer@hevs.ch>
2020-01-24 11:27:41 +0100

    Visualize more complex features (with modalities & labels)

--- src/FeaturesModal.js
+++ src/FeaturesModal.js
@@ -4,6 +4,7 @@ import ListGroup from 'reactstrap/es/ListGroup';
 import Kheops from './services/kheops';
 import { assembleFeatures, assembleFeatureTitles } from './utils/feature-utils';
 import { useKeycloak } from 'react-keycloak';
+import _ from 'lodash';
 
 export default function FeaturesModal({
   isOpen,
@@ -38,10 +39,40 @@ export default function FeaturesModal({
           <ListGroup className="m-1">
             {Object.keys(features).map((key, index) => (
               <ListGroupItem key={key}>
-                <div>{key}</div>
-                <div className="text-muted">
-                  {JSON.stringify(features[key], null, 1)}
+                <div>
+                  <strong>{key}</strong>
                 </div>
+                {!_.isPlainObject(features[key]) ? (
+                  <div className="text-muted">
+                    {JSON.stringify(features[key], null, 1)}
+                  </div>
+                ) : (
+                  Object.keys(features[key]).map((label, idx) => (
+                    <>
+                      <div>{label}</div>
+                      <ListGroup className="m-1">
+                        {Object.keys(features[key][label]).map(
+                          (featureName, index) => (
+                            <ListGroupItem
+                              key={`${key}-${label}-${featureName}`}
+                            >
+                              <div>
+                                <strong>{featureName}</strong>
+                              </div>
+                              <div className="text-muted">
+                                {JSON.stringify(
+                                  features[key][label][featureName],
+                                  null,
+                                  1
+                                )}
+                              </div>
+                            </ListGroupItem>
+                          )
+                        )}
+                      </ListGroup>
+                    </>
+                  ))
+                )}
               </ListGroupItem>
             ))}
           </ListGroup>
